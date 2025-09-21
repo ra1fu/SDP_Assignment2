2398bea0b9418b1bd9b3fff2feb3090e7705d076
Rauan <kuntuganovrauan18@gmail.com>
2025-09-21 14:49:30 +0500

    Update PizzaApp.java

--- src/pizza/app/PizzaApp.java
+++ src/pizza/app/PizzaApp.java
@@ -11,20 +11,21 @@ public class PizzaApp {
 
         // Order a Margherita from the Italian factory
         Pizza margherita = italianFactory.orderPizza("margherita");
-        System.out.println("✅ Ordered: " + margherita.getName());
+        System.out.println("Ordered: " + margherita.getName());
 
         System.out.println("--------------------");
 
         // Order a Pepperoni from the American factory
         Pizza pepperoni = americanFactory.orderPizza("pepperoni");
-        System.out.println("✅ Ordered: " + pepperoni.getName());
+        System.out.println("Ordered: " + pepperoni.getName());
 
         System.out.println("--------------------");
 
         // Attempt to order a pizza type not supported by the factory
         Pizza unknown = italianFactory.orderPizza("hawaiian");
         if (unknown == null) {
-            System.out.println("❌ Sorry, this type of pizza is not available.");
+            System.out.println("Sorry, this type of pizza is not available.");
         }
     }
 }
+
